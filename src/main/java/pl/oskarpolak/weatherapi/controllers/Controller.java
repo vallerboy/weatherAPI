788fde6be24ece78f120ad1b76c3d463829c496b
Oskar <oskarbank@gmail.com>
2017-09-23 16:37:14 +0200

    kk

--- src/main/java/pl/oskarpolak/weatherapi/controllers/Controller.java
+++ src/main/java/pl/oskarpolak/weatherapi/controllers/Controller.java
@@ -1,6 +1,9 @@
 package pl.oskarpolak.weatherapi.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import pl.oskarpolak.weatherapi.models.services.WeatherService;
 
 import java.net.URL;
@@ -10,8 +13,19 @@ public class Controller implements Initializable{
 
     private WeatherService weatherService = WeatherService.getService();
 
+    @FXML
+    Button buttonSend;
+
+    @FXML
+    TextField textCity;
+
 
     public void initialize(URL location, ResourceBundle resources) {
-        weatherService.makeRequest("Cracow");
+       buttonSend.setOnMouseClicked(e -> {
+           if(!textCity.getText().isEmpty()) {
+               weatherService.makeRequest(textCity.getText());
+               textCity.clear();
+           }
+       });
     }
 }
